7eb7ba720216f400b27eb737607b5f3e1baebd6c
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-11-09 14:55:53 +0530

    SRM attendance

---
+++ attendance.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float total_classes,attended_classes,required_classes,current_percent,final_percent,final_total_classes,final_attended_classes;
+    cin>>total_classes>>attended_classes;
+        current_percent = attended_classes/total_classes*100;
+    for (int i = 0;i<50 ; i++)
+    {
+        final_total_classes = total_classes + i;
+        final_attended_classes = attended_classes + i;
+        final_percent= final_attended_classes/final_total_classes*100;
+        if (final_percent>75)
+        {
+            required_classes = final_attended_classes - attended_classes;
+            cout<<"Initial Total classes : "<<total_classes<<endl;
+            cout<<"Initial attended classes : "<<attended_classes<<endl;
+            cout<<"Initial Total classes percentage : "<<current_percent<<"%"<<endl;
+            cout<<"Final Total classes : "<<final_total_classes<<endl;
+            cout<<"Final attended classes : "<<final_attended_classes<<endl;
+            cout<<"Initial Total classes percentage : "<<final_percent<<"%"<<endl;
+            cout<<"Final required classes : "<<required_classes<<endl;
+            break;
+        }
+        
+    }
+    
+    cout<<"Hello";
+    return 0;
+}
