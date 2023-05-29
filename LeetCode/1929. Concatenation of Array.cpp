931a523a955f2f7e136bc4050065620d83167734
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-05-29 23:22:35 +0530

    solved

---
+++ LeetCode/1929. Concatenation of Array.cpp
@@ -0,0 +1,10 @@
+// Problem Link => https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        for(auto it:nums)
+            nums.push_back(it);
+        return nums;
+    }
+};
