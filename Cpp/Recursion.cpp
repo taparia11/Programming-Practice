bb8b14382311c4aecc5dfb5217329778f0bea05d
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-04 23:03:00 +0530

    solved

--- Cpp/Recursion.cpp
+++ Cpp/Recursion.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 int Sum(int);
+int Power(int, int);
+int factorial(int);
 int main()
 {
-    int n;
+    int n, p;
+    // cin >> n ;
+    // cin >> n >> p;
     cin >> n;
-    cout << Sum(n);
+    // cout << Sum(n);
+    // cout << Power(n, p);
+    cout << factorial(n);
     return 0;
 }
 
@@ -14,4 +20,18 @@ int Sum(int n)
     if (n == 0)
         return 0;
     return n + Sum(n - 1);
+}
+
+int Power(int n, int p)
+{
+    if (p == 0)
+        return 1;
+    return n * Power(n, p - 1);
+}
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
 }
