8aeb5a208fae615854e58765bfacdbb0b826fe31
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-06 23:21:08 +0530

    Daily challenge

---
+++ LeetCode/1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,15 @@
+// Problem Link =>  https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/description/
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int diff(arr[1]-arr[0]);
+        for(int i=0;i<arr.size()-1;i++)
+            if(arr[i+1]-arr[i]==diff)
+                continue;
+            else
+                return false;
+        return true;
+    }
+};
