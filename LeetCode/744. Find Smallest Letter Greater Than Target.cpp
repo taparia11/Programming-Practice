ddf7b1c9af50434f61e2b0fe0b72addeea874d79
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-09 21:38:18 +0530

    solved daily challenge

---
+++ LeetCode/744. Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,19 @@
+// Problem Link => https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        char max='z';
+        int c=0;
+        for(auto it:letters)
+            if(it>target)
+                if(max>=it){
+                    max=it;
+                    c++;
+            }
+        if(c==0)
+            return letters[0];
+        else
+            return max;
+    }
+};
