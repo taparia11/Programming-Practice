ef54beed5fd3f7781e8e2022b05c3bd587614157
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-11 15:36:46 +0530

    codechef problem solved

--- CodeChef/The Lead Game.cpp
+++ CodeChef/The Lead Game.cpp
@@ -7,18 +7,27 @@ using namespace std;
 int main(){
     int t;
     cin>>t;
-    int p1[t],p2[t],dp1=0,dp2=0;
+    int p1[t],p2[t],dp=0,dp2=0,x=0,y=0;
     for (int i = 0; i < t; i++)
     {
         cin>>p1[i]>>p2[i];
-        if(p1[i]>p2[i] && p1[i]-p2[i]>dp1)
-            dp1=p1[i]-p2[i];
-        else if(dp2>p2[i]-p1[i])
-            dp2=p2[i]-p1[i];
+        p1[i] = p1[i] + x;
+        p2[i] = p2[i] + y;
+        if(p1[i]>p2[i]){
+            if(dp<p1[i]-p2[i])
+                dp2=1;
+            dp = max(dp,p1[i]-p2[i]);
+            
+        }
+        else{
+            if(dp<p2[i]-p1[i])
+                dp2=2;
+            dp = max(dp,p2[i]-p1[i]);}
+        x=p1[i];
+        y=p2[i];
         
     }
 
-    dp1>dp2?cout<<"1 "<<dp1:cout<<"2 "<<dp2;    
-    
+    cout<<dp2<<" "<<dp;
     return 0;
 }
