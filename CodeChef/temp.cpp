417fb37b5affbb4501d2d0ad179a688c33e2410c
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-11 15:36:55 +0530

    solved leetcode

--- CodeChef/temp.cpp
+++ CodeChef/temp.cpp
@@ -1,40 +1,12 @@
-#include <bits/stdc++.h>
-#include <vector>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    string str;
-    int arr[10]={0};
-    str = to_string(n) + to_string(n*2) + to_string(n*3); 
-    for (int i = 0; i < str.length(); i++)
-    {
-        arr[str[i]] = arr[str[i]] +1 ;
-
-        
-    }
-
-    for(int i=0;i<s.length();i++){
-         for (int j = i; j < s.length(); j++)
-         {
-            /* code */
-            if(s[i]==s[j])
-                return s[i];
-         }
-            
-        }
-    
-    for (int i = 0; i < 10; i++)
-    {
-        if(arr[i]>1){
-            cout<<"false";
-            return 0;
-            }
-    }
-    
-
-    cout<<"true";
-    return 0;
+int main() {
+	int i=1;
+	char c=125;
+	do
+	i++
+	while(c++);
+	cout<<i;
+	return 0;
 }
---
+++ LeetCode/27. Remove Element.cpp
@@ -0,0 +1,26 @@
+// Problem Link => https://leetcode.com/problems/remove-element/
+
+class Solution {
+public:
+    // Two pointer approch
+    int removeElement(vector<int>& nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i]!=val){
+                nums[k] = nums[i];
+                k++;
+            }
+
+    // newbie copy check swap        
+        // int count=0;
+        // vector<int> arr;
+        // for(auto it:nums)
+        //     if(it!=val){
+        //         count++;
+        //         arr.push_back(it);
+        //     }
+        // arr.swap(nums);
+        // return count;
+        return k;
+    }
+};
