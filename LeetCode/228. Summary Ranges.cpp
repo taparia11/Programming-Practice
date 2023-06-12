1593e8c8f7eaf3f0d5592ae0e1afc57edae82af5
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-13 00:19:26 +0530

    updated

--- LeetCode/228. Summary Ranges.cpp
+++ LeetCode/228. Summary Ranges.cpp
@@ -1,2 +1,37 @@
 // Problem Link =>  https://leetcode.com/problems/summary-ranges/description/
 
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int first=0,last=0;
+        vector<string> str;
+        if(nums.empty()){
+            return str;
+        }
+        first=last=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(last==(nums[i]-1)){
+                last=nums[i];
+            }
+            else{
+                if(first!=last){
+                    str.push_back(to_string(first)+"->"+to_string(last));
+                    first=last=nums[i];
+                }
+                else{
+                    str.push_back(to_string(first));
+                    first=last=nums[i];
+                }
+            }
+            
+        }
+        if(first!=last){
+                    str.push_back(to_string(first)+"->"+to_string(last));
+                }
+            else{
+                    str.push_back(to_string(first));
+                }
+
+        return str;
+    }
+};
