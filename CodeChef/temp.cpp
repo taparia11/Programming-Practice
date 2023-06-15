ba5ddff99915eee052a14f90a1341f852a9bc947
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-16 00:46:43 +0530

    solved

--- CodeChef/temp.cpp
+++ CodeChef/temp.cpp
@@ -1,12 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int i=1;
-	char c=125;
-	do
-	i++
-	while(c++);
-	cout<<i;
+int main()
+{
+	string subject;
+    getline(cin,subject);
+
+	// Regex for IPV4
+	regex pattern(
+		"(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})");
+
+	for (regex_iterator<string::iterator> it(
+			subject.begin(), subject.end(), pattern);
+		it != regex_iterator<string::iterator>(); ++it) {
+		cout << it->str() << endl;
+	}
+
+	
+
 	return 0;
 }
