020032408fe8f2b08da6b1fa996ef96a0abfc960
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-16 23:00:43 +0530

    solved

--- CodeChef/temp.cpp
+++ CodeChef/temp.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-	string subject;
-    getline(cin,subject);
+	string subject
+		= "v=spf1 ip4:40.113.200.201 ip6:2001:db8:85a3:8d3:1319:8a2e:370:7348 include:thirdpartydomain.com ~all";
 
 	// Regex for IPV4
 	regex pattern(
@@ -16,7 +16,14 @@ int main()
 		cout << it->str() << endl;
 	}
 
-	
+	regex pattern1(
+		"((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");
+
+	for (regex_iterator<string::iterator> it(
+			subject.begin(), subject.end(), pattern1);
+		it != regex_iterator<string::iterator>(); ++it) {
+		cout << it->str() << endl;
+	}
 
 	return 0;
 }
