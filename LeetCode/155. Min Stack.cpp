014b576c8b034d106ef50b3b80bc99dc28d20ee0
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-21 02:26:50 +0530

    solved using two stacks main and min

---
+++ LeetCode/155. Min Stack.cpp
@@ -0,0 +1,46 @@
+// Problem Link => https://leetcode.com/problems/min-stack/description/
+
+class MinStack {
+public:
+ stack<int> mainSt;
+ stack<int> minSt;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        mainSt.push(val);
+        if(minSt.empty() || val<=minSt.top())
+            minSt.push(val);
+    }
+    
+    void pop() {
+        if(minSt.top()==mainSt.top()){
+            minSt.pop();
+        }
+            mainSt.pop();
+    }
+    
+    int top() {
+       if(!mainSt.empty())
+            return mainSt.top();
+        else
+            return 0;
+    }
+    
+    int getMin() {
+        if(!minSt.empty())
+            return minSt.top();
+        else
+            return 0;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
