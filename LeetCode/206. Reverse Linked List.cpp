c36838a02eb9fc670ae9e9782be5ef91ff723d47
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-26 22:45:39 +0530

    Solved using LinkedList

---
+++ LeetCode/206. Reverse Linked List.cpp
@@ -0,0 +1,33 @@
+// Problem Link => https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* ptr2=head;
+        stack<int> st;
+        while(ptr2!=NULL){
+            st.push(ptr2->val);
+            ptr2 = ptr2->next;
+        }
+        ListNode* ptr1 = new ListNode();
+        ptr2 = ptr1;
+        // head=ptr2;
+        while(!st.empty()){
+            ListNode* ptr = new ListNode(st.top());
+            st.pop();
+            ptr1->next = ptr;
+            ptr1 = ptr1->next;
+        }
+        return ptr2->next;
+    }
+};
