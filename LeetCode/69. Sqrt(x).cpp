d22ecc6978f6f99cae7c9af45bcb6d41ef3a4c78
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-26 22:43:12 +0530

    solved using binary search

---
+++ LeetCode/69. Sqrt(x).cpp
@@ -0,0 +1,20 @@
+// Problem Link => https://leetcode.com/problems/sqrtx/description/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0)
+            return x;
+        int first=1,last=x;
+        while(first<=last){
+            int mid = first + (last-first)/2;
+            if(mid == x/mid)
+                return mid;
+            else if(mid>x/mid)
+                last = --mid;
+            else
+                first = ++mid;
+        }
+        return last;
+    }
+};
