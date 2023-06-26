828d7a553a9798fde309b8740024ca9c43e0435c
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-26 23:31:43 +0530

    Solved using vector

---
+++ LeetCode/234. Palindrome Linked List.cpp
@@ -0,0 +1,30 @@
+// Problem Link => https://leetcode.com/problems/palindrome-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> arr;
+        while(head){
+            arr.emplace_back(head->val);
+            head = head->next;
+        }
+
+        int n = arr.size();
+        int i=0;
+        n--;
+        for(;i<=n;i++,n--)
+            if(arr[i]!=arr[n])
+                return 0;
+        return 1;
+    }
+};
