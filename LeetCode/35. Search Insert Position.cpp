4c2b9642ce0f2a2760644f42cab9163ad640cddf
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-06-28 23:53:40 +0530

    Solved Using binary search

---
+++ LeetCode/35. Search Insert Position.cpp
@@ -0,0 +1,18 @@
+// Problem Link => https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0, end=nums.size()-1,mid;
+        while(start<=end){
+            mid = (start+end)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                end = --mid;
+            else
+                start = ++mid;
+        }
+        return start;
+    }
+};
