cb5fcd2343c02726f758cd3638c7acba79ef0300
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-01 23:18:23 +0530

    Solved

--- LeetCode/136. Single Number.cpp
+++ LeetCode/136. Single Number.cpp
@@ -1,1 +1,15 @@
 // Problem Link => https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> hashmap;
+        for(auto num:nums)
+            hashmap[num]++;
+        for(auto it:hashmap)
+            if(it.second==1){
+                return it.first;
+            }
+        return 0;
+    }
+};
