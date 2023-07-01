0e75d2a416d18a1c1398966a9b2fca24d3da20b5
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-01 23:18:39 +0530

    Solved

--- LeetCode/136. Single Number.cpp
+++ LeetCode/136. Single Number.cpp
@@ -1,13 +1,16 @@
 // Problem Link => https://leetcode.com/problems/single-number/
 
-class Solution {
+class Solution
+{
 public:
-    int singleNumber(vector<int>& nums) {
-        map<int,int> hashmap;
-        for(auto num:nums)
+    int singleNumber(vector<int> &nums)
+    {
+        map<int, int> hashmap;
+        for (auto num : nums)
             hashmap[num]++;
-        for(auto it:hashmap)
-            if(it.second==1){
+        for (auto it : hashmap)
+            if (it.second == 1)
+            {
                 return it.first;
             }
         return 0;
