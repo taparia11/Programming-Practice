362e4ae9fb83be98d6d6caf51e83ea0e87c13db0
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-01 23:18:00 +0530

    Solved

---
+++ LeetCode/217. Contains Duplicate.cpp
@@ -0,0 +1,14 @@
+// Problem Link => https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hashSet;
+        for(int num:nums){
+            if(hashSet.count(num)>0)
+                return true;
+            hashSet.insert(num);
+        }
+        return false;
+    }
+};
