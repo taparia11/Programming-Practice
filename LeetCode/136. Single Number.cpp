c003f5fc96b637d06af81180ed7c4e3b78352c51
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-02 23:48:04 +0530

    Solved using set

--- LeetCode/136. Single Number.cpp
+++ LeetCode/136. Single Number.cpp
@@ -5,14 +5,24 @@ class Solution
 public:
     int singleNumber(vector<int> &nums)
     {
-        map<int, int> hashmap;
-        for (auto num : nums)
-            hashmap[num]++;
-        for (auto it : hashmap)
-            if (it.second == 1)
-            {
-                return it.first;
-            }
-        return 0;
+// Using map STL
+        // map<int, int> hashmap;
+        // for (auto num : nums)
+        //     hashmap[num]++;
+        // for (auto it : hashmap)
+        //     if (it.second == 1)
+        //     {
+        //         return it.first;
+        //     }
+        // return 0;
+
+// Using set STL
+        set<int> hashset;
+        for(auto it:nums)
+            if(hashset.count(it)>0)
+                hashset.erase(it);
+            else 
+                hashset.insert(it);
+        return *(hashset.begin());
     }
 };
