7287baa86e174fd060b5802f92b9edd48edb88f8
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-03 03:21:04 +0530

    Solved using hashing

--- LeetCode/387. First Unique Character in a String.cpp
+++ LeetCode/387. First Unique Character in a String.cpp
@@ -3,15 +3,31 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int siz = s.length();
-        int arr[26]={0};
-        for(int i=0;i<siz;i++){
-                arr[s[i]-97] = arr[s[i]-97] + 1;
+
+    // Using hashing approch
+
+        unordered_map<char , int> mp;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
         }
-        for(int i=0;i<siz;i++){
-                if(arr[s[i]-97] == 1)
-                    return i;
+        for(int i=0;i<n;i++){
+            if(mp[s[i]]==1)
+                return i;
         }
         return -1;
+
+    // Using iterative approch
+
+        // int siz = s.length();
+        // int arr[26]={0};
+        // for(int i=0;i<siz;i++){
+        //         arr[s[i]-97]++;
+        // }
+        // for(int i=0;i<siz;i++){
+        //         if(arr[s[i]-97] == 1)
+        //             return i;
+        // }
+
     }
 };
