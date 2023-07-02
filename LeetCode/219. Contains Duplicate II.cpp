c0c1a5e26a7f92dac3b389b2a996d3156cac1e2a
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-03 04:07:10 +0530

    Solved using hash table

---
+++ LeetCode/219. Contains Duplicate II.cpp
@@ -0,0 +1,20 @@
+// Problem Link => https://leetcode.com/problems/contains-duplicate-ii/description/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            if(mp.count(nums[i])>0){
+                if(abs(mp[nums[i]]-i)<=k)   return true;
+                else mp[nums[i]] = i;
+            }
+            else
+                mp[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
