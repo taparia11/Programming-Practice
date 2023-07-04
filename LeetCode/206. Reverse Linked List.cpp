3bc1b6b9a2d4e83d4b74ecc6d09418f6d839c36f
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-04 22:33:22 +0530

    Using recursion problem solved

--- LeetCode/206. Reverse Linked List.cpp
+++ LeetCode/206. Reverse Linked List.cpp
@@ -12,22 +12,36 @@
  */
 class Solution {
 public:
+
+//Using recursion
     ListNode* reverseList(ListNode* head) {
-        ListNode* ptr2=head;
-        stack<int> st;
-        while(ptr2!=NULL){
-            st.push(ptr2->val);
-            ptr2 = ptr2->next;
-        }
-        ListNode* ptr1 = new ListNode();
-        ptr2 = ptr1;
-        // head=ptr2;
-        while(!st.empty()){
-            ListNode* ptr = new ListNode(st.top());
-            st.pop();
-            ptr1->next = ptr;
-            ptr1 = ptr1->next;
-        }
-        return ptr2->next;
+
+        if(head==NULL || head->next==NULL)
+            return head;
+
+        ListNode* ptr = reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return ptr;
+
+
+//Using Stack
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode* ptr2=head;
+    //     stack<int> st;
+    //     while(ptr2!=NULL){
+    //         st.push(ptr2->val);
+    //         ptr2 = ptr2->next;
+    //     }
+    //     ListNode* ptr1 = new ListNode();
+    //     ptr2 = ptr1;
+    //     // head=ptr2;
+    //     while(!st.empty()){
+    //         ListNode* ptr = new ListNode(st.top());
+    //         st.pop();
+    //         ptr1->next = ptr;
+    //         ptr1 = ptr1->next;
+    //     }
+    //     return ptr2->next;
     }
 };
