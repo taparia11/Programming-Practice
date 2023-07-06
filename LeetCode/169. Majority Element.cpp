6eddfd7d257afa2802e1ef68d1205ac2bbdb3360
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-06 23:33:35 +0530

    Solved using HashMap

--- LeetCode/169. Majority Element.cpp
+++ LeetCode/169. Majority Element.cpp
@@ -1,1 +1,21 @@
 // Problem Link => https://leetcode.com/problems/majority-element/submissions/987999021/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto it:nums)
+            mp[it]++;
+        int max = INT_MIN, val=0;
+        for(auto it:nums){
+
+            if(max<mp[it]){
+
+                val = it;
+                max = mp[it];
+            }
+        }
+
+        return val;
+    }
+};
