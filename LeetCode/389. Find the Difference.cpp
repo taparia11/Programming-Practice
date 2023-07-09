060f9271de5e215b7ed3fe6343c7b860e99f74c8
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-09 20:37:34 +0530

    Solved nlogn

--- LeetCode/389. Find the Difference.cpp
+++ LeetCode/389. Find the Difference.cpp
@@ -1,1 +1,16 @@
 // Problem Link => https://leetcode.com/problems/find-the-difference/description/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char c; 
+        sort(s.begin(), s.end()); 
+        sort(t.begin(), t.end()); 
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == t[i]) continue; 
+            else return t[i];
+        }
+
+        return t[s.length()]; 
+    }
+};
