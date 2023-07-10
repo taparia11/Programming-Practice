d751520ddc67e818eaa5a8cfd5ef143ed4f47a33
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-10 21:55:28 +0530

    Solved using Iteratively

--- LeetCode/700. Search in a Binary Search Tree.cpp
+++ LeetCode/700. Search in a Binary Search Tree.cpp
@@ -1,1 +1,39 @@
 // Problem Link => https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        // Solving Iteratively 
+        TreeNode *temp = root ; 
+
+        while(temp != NULL)
+        {
+            if (temp -> val == val)
+            {
+                return temp ; 
+            }
+
+            if (temp -> val > val)
+            {
+                temp = temp -> left; 
+            }
+
+            else
+            {
+                temp = temp -> right ; 
+            }
+        }
+        return NULL ;  
+    }
+};
