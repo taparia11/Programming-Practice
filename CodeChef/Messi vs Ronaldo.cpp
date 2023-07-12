eb3d2b3bf17557cc5e8716e441def404f0c07db4
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-12 23:58:32 +0530

    Solved

--- CodeChef/Messi vs Ronaldo.cpp
+++ CodeChef/Messi vs Ronaldo.cpp
@@ -1,1 +1,19 @@
 // Problem Link => https://www.codechef.com/problems/MVR
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+    a = a * 2 + b;
+    x = x * 2 + y;
+    if (a > x)
+        cout << "Messi";
+    else if (a < x)
+        cout << "Ronaldo";
+    else
+        cout << "Equal";
+    return 0;
+}
