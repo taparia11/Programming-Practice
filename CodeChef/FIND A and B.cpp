e56335c118af5eb64da20b6531fb66b890848819
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-12 23:59:26 +0530

    Solved

--- CodeChef/FIND A and B.cpp
+++ CodeChef/FIND A and B.cpp
@@ -1,2 +1,25 @@
 // Problem Link => https://www.codechef.com/problems/FINDK3
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if ((a * b) % c == 0)
+            cout << a * b << " " << c;
+        else if ((a * c) % b == 0)
+            cout << a * c << " " << b;
+        else if ((c * b) % a == 0)
+            cout << c * b << " " << a;
+        else
+            cout << -1;
+        cout << endl;
+    }
+    return 0;
+}
