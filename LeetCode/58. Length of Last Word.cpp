63ed101b8a6663c81bcff208961ec58c438409d1
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-13 23:51:55 +0530

    Solved

--- LeetCode/58. Length of Last Word.cpp
+++ LeetCode/58. Length of Last Word.cpp
@@ -1,2 +1,21 @@
 // Problem Link => https://leetcode.com/problems/length-of-last-word/description/
 
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int siz = s.size(), count = 0, flag = 0;
+        for (int i = siz - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ' && flag)
+                break;
+            if (s[i] != ' ')
+            {
+                flag = 1;
+                count++;
+            }
+        }
+        return count;
+    }
+};
