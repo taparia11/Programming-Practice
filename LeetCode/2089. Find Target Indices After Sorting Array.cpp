5d3e128a65ec57c90ef628184d00b27f402bf897
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-15 23:56:46 +0530

    Arranged

--- LeetCode/2089. Find Target Indices After Sorting Array.cpp
+++ LeetCode/2089. Find Target Indices After Sorting Array.cpp
@@ -1,13 +1,15 @@
 // Problem Link => https://leetcode.com/problems/find-target-indices-after-sorting-array/description/
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
+    vector<int> targetIndices(vector<int> &nums, int target)
+    {
         sort(begin(nums), end(nums));
         vector<int> ans;
-        int i = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
-        for(;i<nums.size();i++)
-            if(nums[i]==target)
+        int i = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        for (; i < nums.size(); i++)
+            if (nums[i] == target)
                 ans.emplace_back(i);
 
         return ans;
