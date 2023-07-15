8d404caf201b319151fa66996eaa4ff4e0be6208
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-15 23:56:40 +0530

    Arranged

--- LeetCode/1672. Richest Customer Wealth.cpp
+++ LeetCode/1672. Richest Customer Wealth.cpp
@@ -1,14 +1,18 @@
 // Problem Link => https://leetcode.com/problems/richest-customer-wealth/description/
 
-class Solution {
+class Solution
+{
 public:
-    int maximumWealth(vector<vector<int>>& accounts) {
-        int a=0;
-        for(auto vctr:accounts){
-            for(int i=1;i<vctr.size();i++){
-                vctr[i]+=vctr[i-1];
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+        int a = 0;
+        for (auto vctr : accounts)
+        {
+            for (int i = 1; i < vctr.size(); i++)
+            {
+                vctr[i] += vctr[i - 1];
             }
-            if(*(vctr.rbegin())>a)
+            if (*(vctr.rbegin()) > a)
                 a = *(vctr.rbegin());
         }
         return a;
