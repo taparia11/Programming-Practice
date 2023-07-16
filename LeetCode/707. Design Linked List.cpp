1efb4a71d54ae3ce9560dbdf1f3ce832f95fb897
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-16 23:59:30 +0530

    Arranged

--- LeetCode/707. Design Linked List.cpp
+++ LeetCode/707. Design Linked List.cpp
@@ -1,87 +1,104 @@
 // Problem Link => https://leetcode.com/problems/design-linked-list/description/
 
-class Node{
+class Node
+{
 public:
     int val;
-    Node* next;
-    Node(): val(0),next(NULL){}
-    Node(int data): val(data), next(NULL) {}
+    Node *next;
+    Node() : val(0), next(NULL) {}
+    Node(int data) : val(data), next(NULL) {}
 };
 
-class MyLinkedList {
+class MyLinkedList
+{
 public:
-    Node* head;
-    MyLinkedList() {
+    Node *head;
+    MyLinkedList()
+    {
         head = NULL;
     }
-    
-    int get(int index) {
-        int count=0;
-        Node* ptr=head;
-        while(ptr!=NULL){
-            if(count==index)
+
+    int get(int index)
+    {
+        int count = 0;
+        Node *ptr = head;
+        while (ptr != NULL)
+        {
+            if (count == index)
                 return ptr->val;
-            else{
+            else
+            {
                 count++;
                 ptr = ptr->next;
             }
         }
         return -1;
     }
-    
-    void addAtHead(int val) {
-        Node* ptr = new Node(val);
+
+    void addAtHead(int val)
+    {
+        Node *ptr = new Node(val);
         ptr->next = head;
         head = ptr;
     }
-    
-    void addAtTail(int val) {
-        Node* ptr = new Node(val);
-        if(head==NULL){
-            head=ptr;
+
+    void addAtTail(int val)
+    {
+        Node *ptr = new Node(val);
+        if (head == NULL)
+        {
+            head = ptr;
             return;
         }
-        Node* tptr=head;
-        while(tptr->next!=NULL){
+        Node *tptr = head;
+        while (tptr->next != NULL)
+        {
             tptr = tptr->next;
         }
         tptr->next = ptr;
     }
-    
-    void addAtIndex(int index, int val) {
-        Node* ptr = new Node(val);
-        int count=1;
-        Node* tptr=head;
-        if(index==0){
-            ptr->next=head;
-            head=ptr;
+
+    void addAtIndex(int index, int val)
+    {
+        Node *ptr = new Node(val);
+        int count = 1;
+        Node *tptr = head;
+        if (index == 0)
+        {
+            ptr->next = head;
+            head = ptr;
             return;
         }
-        while(tptr != NULL){
-            if(count==index){
+        while (tptr != NULL)
+        {
+            if (count == index)
+            {
                 ptr->next = tptr->next;
                 tptr->next = ptr;
-                return; 
+                return;
             }
             count++;
             tptr = tptr->next;
-            
         }
     }
-    
-    void deleteAtIndex(int index) {
-        Node* ptr=head;
-        int count=0;
-        if(count==index){
-                head=ptr->next;
-                return;
-            }
-        while(ptr->next!=NULL){
-            if(count+1==index){
+
+    void deleteAtIndex(int index)
+    {
+        Node *ptr = head;
+        int count = 0;
+        if (count == index)
+        {
+            head = ptr->next;
+            return;
+        }
+        while (ptr->next != NULL)
+        {
+            if (count + 1 == index)
+            {
                 ptr->next = ptr->next->next;
                 return;
             }
-            ptr=ptr->next;
+            ptr = ptr->next;
             count++;
         }
     }
