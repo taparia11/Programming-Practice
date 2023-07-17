f1f0a9bf55bf7470dfc30c111ff2b45bda9e8087
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-17 20:22:40 +0530

    Arranged

--- LeetCode/682. Baseball Game.cpp
+++ LeetCode/682. Baseball Game.cpp
@@ -1,32 +1,40 @@
 // Problem Link => https://leetcode.com/problems/baseball-game/
 
-class Solution {
+class Solution
+{
 public:
-    int calPoints(vector<string>& opr) {
+    int calPoints(vector<string> &opr)
+    {
         stack<int> st;
-        int val,val1,ans=0;
-        for(string it:opr){
-            if(it=="C"){
+        int val, val1, ans = 0;
+        for (string it : opr)
+        {
+            if (it == "C")
+            {
                 st.pop();
             }
-            else if(it=="D"){
-                st.push(st.top()*2);
+            else if (it == "D")
+            {
+                st.push(st.top() * 2);
             }
-            else if(it=="+"){
+            else if (it == "+")
+            {
                 val = st.top();
                 st.pop();
-                val1= st.top();
+                val1 = st.top();
                 st.push(val);
-                st.push(val+val1);
+                st.push(val + val1);
             }
-            else{
+            else
+            {
                 st.push(stoi(it));
             }
         }
-        while(!st.empty()){
-                    ans+=st.top();
-                    st.pop();
-                }
+        while (!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
         return ans;
     }
 };
