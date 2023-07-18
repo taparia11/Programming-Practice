616e8cb069bad125fac7f86abc088a77545c42f2
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-18 22:43:02 +0530

    Arranged

--- LeetCode/430. Flatten a Multilevel Doubly Linked List.cpp
+++ LeetCode/430. Flatten a Multilevel Doubly Linked List.cpp
@@ -11,25 +11,30 @@ public:
 };
 */
 
-class Solution {
+class Solution
+{
 public:
-
-    Node* merge(Node* list){
-        while(list->next!=NULL)
+    Node *merge(Node *list)
+    {
+        while (list->next != NULL)
             list = list->next;
         return list;
     }
 
-    Node* flatten(Node* head) {
+    Node *flatten(Node *head)
+    {
         Node *ptr = head;
-        if(!head)  return ptr;
+        if (!head)
+            return ptr;
 
-        while(ptr!=NULL){
-            if(ptr->child!=NULL){
-                Node* temp = merge(ptr->child);
+        while (ptr != NULL)
+        {
+            if (ptr->child != NULL)
+            {
+                Node *temp = merge(ptr->child);
                 temp->next = ptr->next;
-                if(ptr->next!=NULL)
-                ptr->next->prev = temp;
+                if (ptr->next != NULL)
+                    ptr->next->prev = temp;
                 ptr->next = ptr->child;
                 ptr->child->prev = ptr;
                 ptr->child = NULL;
@@ -37,10 +42,11 @@ public:
             ptr = ptr->next;
         }
         ptr = head;
-        while(ptr){
+        while (ptr)
+        {
             // if(ptr->child!=NULL)
             ptr->child = NULL;
-            ptr=ptr->next;
+            ptr = ptr->next;
         }
         return head;
     }
