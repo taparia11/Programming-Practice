4565cef1bf37fb911cde4cdc4a5832df4df6b46a
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-19 23:07:41 +0530

    Arranged

--- LeetCode/228. Summary Ranges.cpp
+++ LeetCode/228. Summary Ranges.cpp
@@ -1,36 +1,45 @@
 // Problem Link =>  https://leetcode.com/problems/summary-ranges/description/
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> summaryRanges(vector<int>& nums) {
-        int first=0,last=0;
+    vector<string> summaryRanges(vector<int> &nums)
+    {
+        int first = 0, last = 0;
         vector<string> str;
-        if(nums.empty()){
+        if (nums.empty())
+        {
             return str;
         }
-        first=last=nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(last==(nums[i]-1)){
-                last=nums[i];
+        first = last = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (last == (nums[i] - 1))
+            {
+                last = nums[i];
             }
-            else{
-                if(first!=last){
-                    str.push_back(to_string(first)+"->"+to_string(last));
-                    first=last=nums[i];
+            else
+            {
+                if (first != last)
+                {
+                    str.push_back(to_string(first) + "->" + to_string(last));
+                    first = last = nums[i];
                 }
-                else{
+                else
+                {
                     str.push_back(to_string(first));
-                    first=last=nums[i];
+                    first = last = nums[i];
                 }
             }
-            
         }
-        if(first!=last){
-                    str.push_back(to_string(first)+"->"+to_string(last));
-                }
-            else{
-                    str.push_back(to_string(first));
-                }
+        if (first != last)
+        {
+            str.push_back(to_string(first) + "->" + to_string(last));
+        }
+        else
+        {
+            str.push_back(to_string(first));
+        }
 
         return str;
     }
