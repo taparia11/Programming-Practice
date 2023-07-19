5f09c3d08f98834d72bcc9d91fc5627e5ddcb9d2
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-19 23:07:32 +0530

    Arranged

--- LeetCode/206. Reverse Linked List.cpp
+++ LeetCode/206. Reverse Linked List.cpp
@@ -10,38 +10,38 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
+    // Using recursion
+    ListNode *reverseList(ListNode *head)
+    {
 
-//Using recursion
-    ListNode* reverseList(ListNode* head) {
-
-        if(head==NULL || head->next==NULL)
+        if (head == NULL || head->next == NULL)
             return head;
 
-        ListNode* ptr = reverseList(head->next);
-        head->next->next=head;
-        head->next=NULL;
+        ListNode *ptr = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
         return ptr;
 
-
-//Using Stack
-    // ListNode* reverseList(ListNode* head) {
-    //     ListNode* ptr2=head;
-    //     stack<int> st;
-    //     while(ptr2!=NULL){
-    //         st.push(ptr2->val);
-    //         ptr2 = ptr2->next;
-    //     }
-    //     ListNode* ptr1 = new ListNode();
-    //     ptr2 = ptr1;
-    //     // head=ptr2;
-    //     while(!st.empty()){
-    //         ListNode* ptr = new ListNode(st.top());
-    //         st.pop();
-    //         ptr1->next = ptr;
-    //         ptr1 = ptr1->next;
-    //     }
-    //     return ptr2->next;
+        // Using Stack
+        //  ListNode* reverseList(ListNode* head) {
+        //      ListNode* ptr2=head;
+        //      stack<int> st;
+        //      while(ptr2!=NULL){
+        //          st.push(ptr2->val);
+        //          ptr2 = ptr2->next;
+        //      }
+        //      ListNode* ptr1 = new ListNode();
+        //      ptr2 = ptr1;
+        //      // head=ptr2;
+        //      while(!st.empty()){
+        //          ListNode* ptr = new ListNode(st.top());
+        //          st.pop();
+        //          ptr1->next = ptr;
+        //          ptr1 = ptr1->next;
+        //      }
+        //      return ptr2->next;
     }
 };
