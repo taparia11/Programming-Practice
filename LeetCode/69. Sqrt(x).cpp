33d8fa80a080e9f0360b061b76f5f6c8f121228b
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-20 21:46:57 +0530

    Arranged

--- LeetCode/69. Sqrt(x).cpp
+++ LeetCode/69. Sqrt(x).cpp
@@ -1,16 +1,19 @@
 // Problem Link => https://leetcode.com/problems/sqrtx/description/
 
-class Solution {
+class Solution
+{
 public:
-    int mySqrt(int x) {
-        if(x==0)
+    int mySqrt(int x)
+    {
+        if (x == 0)
             return x;
-        int first=1,last=x;
-        while(first<=last){
-            int mid = first + (last-first)/2;
-            if(mid == x/mid)
+        int first = 1, last = x;
+        while (first <= last)
+        {
+            int mid = first + (last - first) / 2;
+            if (mid == x / mid)
                 return mid;
-            else if(mid>x/mid)
+            else if (mid > x / mid)
                 last = --mid;
             else
                 first = ++mid;
