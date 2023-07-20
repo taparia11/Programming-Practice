0c107d4529529d2e9a2e69c37b3373cb2a5e97cf
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-20 21:47:05 +0530

    Arranged

--- LeetCode/94. Binary Tree Inorder Traversal.cpp
+++ LeetCode/94. Binary Tree Inorder Traversal.cpp
@@ -11,10 +11,10 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-
-// commented is recursive type solution
+    // commented is recursive type solution
     // vector<int> tree;
     // void inorder(TreeNode* root){
     //     if(root==NULL)
@@ -23,21 +23,25 @@ public:
     //     tree.push_back(root->val);
     //     inorder(root->right);
     // }
-    vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> inorderTraversal(TreeNode *root)
+    {
         // inorder(root);
         // return tree;
-        if(root==NULL)
+        if (root == NULL)
             return {};
         vector<int> inorder;
-        stack<TreeNode*> st;
-        TreeNode* node = root;
-        while(true){
-            if(node!=NULL){
+        stack<TreeNode *> st;
+        TreeNode *node = root;
+        while (true)
+        {
+            if (node != NULL)
+            {
                 st.push(node);
                 node = node->left;
             }
-            else{
-                if(st.empty()==true)
+            else
+            {
+                if (st.empty() == true)
                     break;
                 node = st.top();
                 st.pop();
