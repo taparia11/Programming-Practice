dab7a13526dd97cabc6e434f5eda59be12a45bfe
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-20 21:46:59 +0530

    Arranged

--- LeetCode/73. Set Matrix Zeroes.cpp
+++ LeetCode/73. Set Matrix Zeroes.cpp
@@ -1,24 +1,30 @@
 // Problem Link => https://leetcode.com/problems/set-matrix-zeroes/description/
 
-class Solution {
+class Solution
+{
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> vec;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0)
-                    vec.push_back({i,j});
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        vector<pair<int, int>> vec;
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[i].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                    vec.push_back({i, j});
             }
         }
-        for(auto it:vec){
-            for(int i=0;i<matrix[it.first].size();i++){
-                matrix[it.first][i]=0;
+        for (auto it : vec)
+        {
+            for (int i = 0; i < matrix[it.first].size(); i++)
+            {
+                matrix[it.first][i] = 0;
             }
-               for(int i=0;i<matrix.size();i++){
-                matrix[i][it.second]=0;
+            for (int i = 0; i < matrix.size(); i++)
+            {
+                matrix[i][it.second] = 0;
             }
         }
-            // cout<<it.first<<" "<<it.second;
-
+        // cout<<it.first<<" "<<it.second;
     }
 };
