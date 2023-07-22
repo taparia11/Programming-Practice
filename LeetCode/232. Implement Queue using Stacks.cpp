7f1906ee7bfac9c62501cdebfc202fe8a1bcb9fb
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-22 21:31:54 +0530

    arranged

--- LeetCode/232. Implement Queue using Stacks.cpp
+++ LeetCode/232. Implement Queue using Stacks.cpp
@@ -1,49 +1,58 @@
 // Problem Link => https://leetcode.com/problems/implement-queue-using-stacks/
 
-class MyQueue {
+class MyQueue
+{
 public:
     stack<int> st1;
     stack<int> st2;
-    MyQueue() {
-        
+    MyQueue()
+    {
     }
-    
-    void push(int x) {
+
+    void push(int x)
+    {
         st1.push(x);
     }
-    
-    int pop() {
-        int s = st1.size()-1;
-        while(s--){
+
+    int pop()
+    {
+        int s = st1.size() - 1;
+        while (s--)
+        {
             st2.push(st1.top());
             st1.pop();
         }
         s = st1.top();
         st1.pop();
-        while(!st2.empty()){
+        while (!st2.empty())
+        {
             st1.push(st2.top());
             st2.pop();
         }
         return s;
     }
-    
-    int peek() {
-        int s = st1.size()-1;
-        while(s--){
+
+    int peek()
+    {
+        int s = st1.size() - 1;
+        while (s--)
+        {
             st2.push(st1.top());
             st1.pop();
         }
         s = st1.top();
         st2.push(st1.top());
         st1.pop();
-        while(!st2.empty()){
+        while (!st2.empty())
+        {
             st1.push(st2.top());
             st2.pop();
         }
         return s;
     }
-    
-    bool empty() {
+
+    bool empty()
+    {
         return st1.empty();
     }
 };
