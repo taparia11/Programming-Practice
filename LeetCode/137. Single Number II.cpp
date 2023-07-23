4a4cc929740910a83e56bd58aba1e3bf128b3331
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-23 23:28:44 +0530

    Arranged

--- LeetCode/137. Single Number II.cpp
+++ LeetCode/137. Single Number II.cpp
@@ -1,13 +1,15 @@
 // Problem Link => https://leetcode.com/problems/single-number-ii/
 
-class Solution {
+class Solution
+{
 public:
-    int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(auto num:nums)
+    int singleNumber(vector<int> &nums)
+    {
+        unordered_map<int, int> mp;
+        for (auto num : nums)
             mp[num]++;
-        for(auto i=mp.begin();i!=mp.end();i++)
-            if(i->second==1)
+        for (auto i = mp.begin(); i != mp.end(); i++)
+            if (i->second == 1)
                 return i->first;
         return -1;
     }
