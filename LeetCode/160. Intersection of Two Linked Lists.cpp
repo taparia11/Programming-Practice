e660f004db7a33d724ff738c51b1d17585550758
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-25 19:27:20 +0530

    Arranged

--- LeetCode/160. Intersection of Two Linked Lists.cpp
+++ LeetCode/160. Intersection of Two Linked Lists.cpp
@@ -8,54 +8,62 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* ptr1 = headA;
-        ListNode* ptr2 = headB;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        ListNode *ptr1 = headA;
+        ListNode *ptr2 = headB;
 
-        int cl1=0,cl2=0;
-        while(ptr1){
+        int cl1 = 0, cl2 = 0;
+        while (ptr1)
+        {
             ptr1 = ptr1->next;
             cl1++;
         }
-        while(ptr2){
+        while (ptr2)
+        {
             ptr2 = ptr2->next;
             cl2++;
         }
 
-        if(cl1==cl2 && headA==headB)
+        if (cl1 == cl2 && headA == headB)
             return headA;
 
-        if(cl1>=cl2){
-            cl1 = cl1-cl2;
-            ptr1 =headA;
-            ptr2 =headB;
-            while(cl1--)
+        if (cl1 >= cl2)
+        {
+            cl1 = cl1 - cl2;
+            ptr1 = headA;
+            ptr2 = headB;
+            while (cl1--)
                 ptr1 = ptr1->next;
-            while(ptr1!=ptr2){
+            while (ptr1 != ptr2)
+            {
                 ptr1 = ptr1->next;
                 ptr2 = ptr2->next;
             }
             // return ptr2;
-            if(ptr1==NULL || ptr2==NULL)
+            if (ptr1 == NULL || ptr2 == NULL)
                 return NULL;
             else
                 return ptr1;
             // if(ptr1->next->val==ptr2->next->val)
         }
-        else{
-            cl2 = cl2-cl1;
-            ptr1 =headA;
-            ptr2 =headB;
-            while(cl2--)
+        else
+        {
+            cl2 = cl2 - cl1;
+            ptr1 = headA;
+            ptr2 = headB;
+            while (cl2--)
                 ptr2 = ptr2->next;
-            while(ptr1!=ptr2){
+            while (ptr1 != ptr2)
+            {
                 ptr1 = ptr1->next;
                 ptr2 = ptr2->next;
             }
 
-            if(ptr1==NULL || ptr2==NULL)
+            if (ptr1 == NULL || ptr2 == NULL)
                 return NULL;
             else
                 return ptr1;
