705e5ea6f53f483bbd6308c0df0ea7c7fb6a118b
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-26 21:13:37 +0530

    Arranged

--- LeetCode/2. Add Two Numbers.cpp
+++ LeetCode/2. Add Two Numbers.cpp
@@ -10,72 +10,81 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ptr1=l1;
-        ListNode* ptr2=l2;
-        ListNode* ptr3 = new ListNode(0);
-        
-        int carry=0;
-        ListNode* ans = ptr3;
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *ptr1 = l1;
+        ListNode *ptr2 = l2;
+        ListNode *ptr3 = new ListNode(0);
 
-        while(ptr1!=NULL && ptr2!=NULL){
+        int carry = 0;
+        ListNode *ans = ptr3;
+
+        while (ptr1 != NULL && ptr2 != NULL)
+        {
             int val1 = ptr1->val;
             int val2 = ptr2->val;
-            int val = val1+val2+carry;
-            carry = val/10;
-            ListNode* temp = new ListNode(val%10);
+            int val = val1 + val2 + carry;
+            carry = val / 10;
+            ListNode *temp = new ListNode(val % 10);
             ptr1 = ptr1->next;
             ptr2 = ptr2->next;
             ptr3->next = temp;
             ptr3 = ptr3->next;
         }
-        if(carry!=0){
-            ListNode* temp = new ListNode(carry);
-            ptr3->next = temp;
-        }
-        if(ptr1==NULL && ptr2==NULL)
-            return ans->next;
-        else if(ptr1==NULL && ptr2!=NULL)
+        if (carry != 0)
         {
-            while(carry!=0 && ptr2!=nullptr){
-            int val =ptr2->val+carry;
-            carry = val/10;
-            ListNode* temp = new ListNode(val%10);
-            ptr2 = ptr2->next;
+            ListNode *temp = new ListNode(carry);
             ptr3->next = temp;
-            ptr3 = ptr3->next;
+        }
+        if (ptr1 == NULL && ptr2 == NULL)
+            return ans->next;
+        else if (ptr1 == NULL && ptr2 != NULL)
+        {
+            while (carry != 0 && ptr2 != nullptr)
+            {
+                int val = ptr2->val + carry;
+                carry = val / 10;
+                ListNode *temp = new ListNode(val % 10);
+                ptr2 = ptr2->next;
+                ptr3->next = temp;
+                ptr3 = ptr3->next;
             }
-            if(carry==0)
+            if (carry == 0)
                 ptr3->next = ptr2;
-            else{
-                ListNode* temp = new ListNode(carry);
+            else
+            {
+                ListNode *temp = new ListNode(carry);
                 ptr3->next = temp;
             }
         }
-        else if(ptr1!=NULL && ptr2==NULL){
+        else if (ptr1 != NULL && ptr2 == NULL)
+        {
             // cout<<ptr1->val;
-            while(carry!=0 && ptr1!=nullptr){
-            int val = ptr1->val;
-            val = val + carry;
-            carry = val/10;
-            ListNode* temp = new ListNode(val%10);
-            ptr1 = ptr1->next;
-            ptr3->next = temp;
-            ptr3 = ptr3->next;
+            while (carry != 0 && ptr1 != nullptr)
+            {
+                int val = ptr1->val;
+                val = val + carry;
+                carry = val / 10;
+                ListNode *temp = new ListNode(val % 10);
+                ptr1 = ptr1->next;
+                ptr3->next = temp;
+                ptr3 = ptr3->next;
             }
-            if(carry==0)
+            if (carry == 0)
                 ptr3->next = ptr1;
-            else{
-                ListNode* temp = new ListNode(carry);
+            else
+            {
+                ListNode *temp = new ListNode(carry);
                 ptr3->next = temp;
             }
         }
-        else 
+        else
             ptr3->next = ptr1;
 
-            // cout<<ans->val;
+        // cout<<ans->val;
         return ans->next;
     }
 };
