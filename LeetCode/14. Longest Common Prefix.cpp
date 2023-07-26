681df7cfed83f6928bce5e8fcff6121a4f2834c0
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-26 21:13:43 +0530

    Arranged

--- LeetCode/14. Longest Common Prefix.cpp
+++ LeetCode/14. Longest Common Prefix.cpp
@@ -1,34 +1,42 @@
 // Problem Link => https://leetcode.com/problems/longest-common-prefix/
 
-class Solution {
+class Solution
+{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string s="";
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string s = "";
         int n = strs.size();
         int arr[n];
-        int minlen=INT_MAX;
-        for(int i=0;i<n;i++){
+        int minlen = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
             arr[i] = strs[i].length();
-            minlen = min(minlen,arr[i]);
+            minlen = min(minlen, arr[i]);
         }
-        if(n==1){
-            s= strs[0];
+        if (n == 1)
+        {
+            s = strs[0];
             return s;
         }
-        int count=0,res=INT_MAX;
-        for(int i=0;i<1;i++){
-            for(int j =1;j<n;j++){
-                count=0;
-                for(int k =0;k<minlen;k++){
-               if(strs[i][k]==strs[j][k])
-                    count++;
-                else
-                    break;}
-                res = min(res,count);
+        int count = 0, res = INT_MAX;
+        for (int i = 0; i < 1; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                count = 0;
+                for (int k = 0; k < minlen; k++)
+                {
+                    if (strs[i][k] == strs[j][k])
+                        count++;
+                    else
+                        break;
+                }
+                res = min(res, count);
             }
         }
-        for(int i=0;i<res;i++)
-            s= s+ strs[0][i];
+        for (int i = 0; i < res; i++)
+            s = s + strs[0][i];
         return s;
     }
 };
