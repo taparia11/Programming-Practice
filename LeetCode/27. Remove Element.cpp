80f0be9ef4cff16021746f4ec919dfb6ccc49d28
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-27 23:50:16 +0530

    Arranged

--- LeetCode/27. Remove Element.cpp
+++ LeetCode/27. Remove Element.cpp
@@ -1,17 +1,20 @@
 // Problem Link => https://leetcode.com/problems/remove-element/
 
-class Solution {
+class Solution
+{
 public:
     // Two pointer approch
-    int removeElement(vector<int>& nums, int val) {
-        int k=0;
-        for(int i=0;i<nums.size();i++)
-            if(nums[i]!=val){
+    int removeElement(vector<int> &nums, int val)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] != val)
+            {
                 nums[k] = nums[i];
                 k++;
             }
 
-    // newbie copy check swap        
+        // newbie copy check swap
         // int count=0;
         // vector<int> arr;
         // for(auto it:nums)
