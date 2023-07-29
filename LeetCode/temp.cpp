b8e11f34d3ed76125435c9fa0a26622bc1c8615e
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-29 20:24:40 +0530

    codeforces contest

--- LeetCode/temp.cpp
+++ LeetCode/temp.cpp
@@ -4,33 +4,34 @@
 using namespace std;
 
 // Funtion for mex alternating subarray legth
-int max_alternating_subarray_length(vector<int> nums) {
-  int max_length = 1;
-  int current_length = 1;
-  int prev_num = nums[0];
-  for (int i = 1; i < nums.size(); i++) {
-    if ((nums[i - 1] ^ nums[i]) == 1) {
-      current_length++;
-    } else {
-      current_length = 1;
-    }
-    max_length = max(max_length, current_length);
-  }
-  return max_length == 1 ? -1 : max_length;
-}
-
-//Driver code
-int main() {
-  vector<int> nums;
-  int a,n;
-  cin>>n;
-  for (int i = 0; i < n; i++)
-  {
-    cin>>a;
-    nums.push_back(a);
+int max_alternating_subarray_length(vector<int> nums)
+{
+  long long int i,count=0;
+  for( i=1;i<=nums.size();i++){
+    if(nums[i-1]==i)
+      count++;
   }
   
-  int max_length = max_alternating_subarray_length(nums);
-  cout << max_length << endl;
+  return count>1?count-1:count==1?1:0;
+}
+
+// Driver code
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--){
+    long long int a, n;
+    cin >> n;
+    vector< int> nums;
+    for (int i = 0; i < n; i++)
+    {
+      cin >> a;
+      nums.push_back(a);
+    }
+
+    long long int max_length = max_alternating_subarray_length(nums);
+    cout << max_length << endl;
+  }
   return 0;
 }
