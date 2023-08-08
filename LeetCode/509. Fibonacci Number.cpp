0248bece38269ce776bb2f1c1ec0d44af5ac95c6
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-08-08 12:21:40 +0530

    Solved on Leetcode comment added

--- LeetCode/509. Fibonacci Number.cpp
+++ LeetCode/509. Fibonacci Number.cpp
@@ -3,8 +3,10 @@
 class Solution {
 public:
     int fib(int n) {
+        //if n is 0 base case
         if(n==0)
             return 0;
+        //if n is 1 base case
         if(n==1)
             return 1;
         return fib(n-1)+fib(n-2);
