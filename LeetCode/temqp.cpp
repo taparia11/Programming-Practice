dd316fb1ed3ab40a27ba48dfba1e50989a7e291b
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-08-10 01:11:10 +0530

    Solved on Leetcode recursion sum can take value multiple times

--- LeetCode/temqp.cpp
+++ LeetCode/temqp.cpp
@@ -2,14 +2,48 @@
 #include<vector>
 using namespace std;
 
-int main(){
-    vector<int> ans;
-    ans.emplace_back(5);
-    ans.emplace_back(7);
-    ans.emplace_back(2);
-    ans.emplace_back(9);
+void findCombinations(int idx, int target, vector<int> &a, vector<vector<int>> &ans, vector<int> &ds){
+    cout<<a.size()<<endl;
+    if(idx == a.size()){
+        if(target==0){
+            // cout<<target<<endl;
+            ans.push_back(ds);
+        }
+        return;
+    }
+    if(a[idx] <= target){
+        ds.push_back(a[idx]);
+        findCombinations(idx, target - a[idx], a, ans, ds);
+        ds.pop_back();
+    }
+    findCombinations(idx + 1, target, a, ans, ds);
+}
 
-    for(auto it: ans)
-        cout<<it<<endl;
+vector<vector<int>> subarraysWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    vector<vector<int>> ans;
+    vector<int> ds;
+    findCombinations(0, k, a, ans, ds);
+    return ans;
+}
+
+int main(){
+    int k, n;
+    cin>>n>>k;
+    vector<int> a;
+    for(int i=0; i<n; i++){
+        int val;
+        cin>>val;
+        a.push_back(val);
+    }
+
+    vector<vector<int>> res;
+    res = subarraysWithSumK(a, k);
+    for(auto it:res){
+        for (auto i : it)
+            cout<<i<<" ";    
+        cout<<endl;
+    }
+
     return 0;
 }
