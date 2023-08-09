b38eca2f2ec13fea95daf9b7989d93f2555445d5
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-08-09 11:01:40 +0530

    Solved on Leetcode recursion sum take or leave value

--- LeetCode/temp.cpp
+++ LeetCode/temp.cpp
@@ -1,37 +1,38 @@
-#include <iostream>
-#include <vector>
-
+#include<bits/stdc++.h>
+#include<vector>
 using namespace std;
 
-// Funtion for mex alternating subarray legth
-int swapCount(vector<int> nums)
-{
-  int count=0;
-  for(int i=1;i<=nums.size();i++){
-    if(nums[i-1]==i)
-      count++;
-  }
-  
-  return count>1?count-1:count==1?1:0;
+void print(vector<int> &res){
+    for(int it:res)
+      cout<<it<<" ";
+    cout<<endl;
 }
 
-// Driver code
-int main()
-{
-  int t;
-  cin >> t;
-  while(t--){
-    int a, n;
-    cin >> n;
-    vector<int> nums;
-    for (int i = 0; i < n; i++)
-    {
-      cin >> a;
-      nums.push_back(a);
-    }
-
-    int max_Swap = swapCount(nums);
-    cout << max_Swap << endl;
+void subSequences(int k, int idx, int n, vector<int> &values, vector<int> &res, int &sum){
+  if(idx>=n){
+    if(sum == k)
+      print(res);
+    return;
   }
+  res.push_back(values[idx]);
+  sum += values[idx];
+  subSequences(k, idx + 1, n, values, res, sum);
+  res.pop_back();
+  sum -= values[idx];
+  subSequences(k, idx + 1, n, values, res, sum);
+}
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> values,res;
+  for(int i=0; i<n; i++){
+    int val;
+    cin>>val;
+    values.emplace_back(val);
+  }
+  int k,sum=0;
+  cin>>k;
+  subSequences(k, 0, n , values, res, sum);
   return 0;
 }
