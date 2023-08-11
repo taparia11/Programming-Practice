58bcbc25a3e0247191b6022441e6c866bdde61a2
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-08-11 21:12:50 +0530

    cpp concept practice

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -1,52 +1,29 @@
 #include<bits/stdc++.h>
 #include<vector>
-#include<string>
 using namespace std;
 
-// static int count = 1;
-
-class student{
-    private:
-        int A,B;
-        string Full_Name;
-    public:
-
-        student(string name){
-            this->Full_Name = name;
-            this->A = 5;
-            B = 7;
-            cout<<"Object Created "<<this->Full_Name<<endl;
-        }
-
-        void getName(string name){
-            this->Full_Name = name;
-            // cin>>this->Full_Name;
-            // getline(cin, Full_Name);
-        }
-
-        void printName();
-
-        void print(){
-            cout<<this->B<<" "<<A<<endl;
-        }
-
-        ~student(){
-            cout<<"Object removed"<<this->Full_Name<<endl;
-        }
-};
-
-void student::printName(){
-    cout<<endl<<this->Full_Name<<endl;
-}
-
 int main(){
-    student s1("Nikhil");
-    s1.print();
-    // s1.getName();
-    s1.printName();
-    student s2("taparia");
-    s2.print();
-    // s2.getName();
-    s2.printName();
+    int n;
+    cin>>n;
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin>>val;
+        ans.push_back(val);
+    }
+bool flag = true;
+    for (int i = 1; i < n; i++)
+    {
+        /* code */if(ans[i-1] != ans[i]){
+                cout<<"No";
+                flag = false;
+        }
+    }
+
+if(flag)
+    cout<<"Yes";
+    
+    
     return 0;
 }
