089081f1dd5eb142fdbad6f31c3aad56227be729
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-08-21 10:29:10 +0530

    Solved on Coding studios code dp comment added

--- LeetCode/Minimal Cost.cpp
+++ LeetCode/Minimal Cost.cpp
@@ -1,21 +1,26 @@
 // Problem Link => https://www.codingninjas.com/studio/problems/minimal-cost_8180930?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
 
 int minimizeCost(int n, int k, vector<int> &height){
-    // Write your code here.
+    // dp Initialized
     vector<int> dp(n,0);
+    //base case
     dp[0] = 0;
+    // iterating till nth stone
     for(int i=1; i<n; i++){
         int sum = INT_MAX;
+        // from every stone to jumping till k and checking energy loss
         for(int j=1; j<=k; j++){
             int jump;
+            // if index is possiable then only calculate energy
             if(i-j >= 0){
                 jump = dp[i-j] + abs(height[i] - height[i-j]);
+                // taking minimum of all jumps 
                 sum = min(sum, jump);
             }
-            else
-                continue;
         }
+        // storing minimum energy value to dp;
         dp[i] = sum;
     }
+    // returning till nth index minimum energy loss 
     return dp[n-1];
 }
