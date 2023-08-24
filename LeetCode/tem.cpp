748b3361bcc6ed6ed4ad69c554896a54efb6ee46
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-08-25 01:02:40 +0530

    Hackerearth OT OR query for mayank and sid

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -2,117 +2,46 @@
 #include<vector>
 using namespace std;
 
-int check(int i, int n, string s){
-	int checkVal = i;
-	while(checkVal<n){
-		if(s[checkVal] <= 122 && s[checkVal] >= 97){
-			break;
-		}
-		else
-			checkVal++;	
-	}
-	return checkVal;
-}
-
-void solve(int idx, int n, string s, int &count, int dots, bool &posible){
-	if(idx >= n){
-		if(dots == 0){
-			count++;
-		}
-			return;
-	}
-	for(int i=idx; i<n; i++){
-		if(s[i] == '.'){
-			if(i == 0){
-				int checkVal = check(i, n, s);
-				if(checkVal == n){
-					int val = 97;
-					if(val <= 0){
-						posible = false;
-						return;
-					}
-					for(int j=0; j<val; j++){
-						s[i] = 97 + j;
-						solve(i+1, n, s, count, dots-1, posible);
-					}
-				}
-				else {
-					int val = s[i+1] - 97;
-					if(val <= 0){
-						posible = false;
-						return;
-					}
-					for(int j=0; j<val; j++){
-						s[i] = 97 + j;
-						solve(i+1, n, s, count, dots-1, posible);
-					}
-				}
-			}
-			else if(i == n-1){
-				int val = 122 - s[i-1];
-				if(val <= 0){
-					posible = false;
-					return;
-				}
-				for(int j=1; j<=val; j++){
-					s[i] = s[i-1] + j;
-					solve(i+1, n, s, count, dots-1, posible);
-				}
-			}
-			else{
-				int checkVal = check(i, n, s);
-				if(checkVal == n){
-					int val = 122 - s[i-1];
-					if(val<=0){
-						posible = false;
-						return;
-					}
-					for(int j = 1; j<=val; j++){
-						s[i] =  s[i-1] + j;
-						solve(i+1, n, s, count, dots-1, posible);
-					}
-				}
-				else{
-					int val = s[checkVal] - s[i-1];
-					if(val<=0){
-						posible = false;
-						return;
-					}
-					for(int j = 1; j<val; j++){
-						s[i] =  s[i-1] + j;
-						solve(i+1, n, s, count, dots-1, posible);
-					}
-				}
-			}
-		}
-		else if(i == n-1){
-			solve(i+1, n, s, count, dots, posible);
-		}
-	}
-}
-
 int main(){
-	int t;
-	cin>>t;
-	while (t--)
-	{
-		int n;
-		cin>>n;
-		string s;
-		cin>>s;
-		int count = 0;
-		int dots = 0;
-		bool posible = true;
-		for(int i=0; i<n; i++){
-			if(s[i] == '.')
-				dots++;
-		}
-		solve(0, n, s, count, dots, posible);
-		// if(posible == false)
-		// 	cout<<"0\n";
-		// else
-			cout<<count<<endl;
+	int n;
+	cin>>n;
+	int A[n], B[n];
+	for(int i=0; i<n; i++){
+		cin>>A[i];
 	}
-	
+	for(int i=0; i<n; i++){
+		cin>>B[i];
+	}
+	int M;
+	cin>>M;
+	int L[M], R[M], P[M], Q[M];
+	for(int i=0; i<M; i++){
+		cin>>L[i];
+	}
+	for(int i=0; i<M; i++){
+		cin>>R[i];
+	}
+	for(int i=0; i<M; i++){
+		cin>>P[i];
+	}
+	for(int i=0; i<M; i++){
+		cin>>Q[i];
+	}
+	int temp = M;
+
+	for(int i=0; i<M; i++){
+	int sum = 0;
+		for (int j = L[i]; j <= R[i] ; j++)
+		{
+			for (int k = P[i]; k <= Q[i]; k++)
+			{
+				sum += A[j-1] | B[k-1];
+			}
+			
+		}
+		cout<<sum<<" ";
+		
+	}
+
 	return 0;
 }
