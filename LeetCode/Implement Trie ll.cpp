0b3bd7dac34be724c9eece550eab573c0a3c41e9
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-09-04 01:01:10 +0530

    Solved on Coding Ninjas Trie and added Comments

--- LeetCode/Implement Trie ll.cpp
+++ LeetCode/Implement Trie ll.cpp
@@ -4,43 +4,51 @@
 
 struct Node{
     Node* links[26];
-    int endWith = 0;
-    int countPrefix = 0;
+    int endWith = 0;    // when a word will end at that moment it will incremented.
+    int countPrefix = 0;    // It will contains how many words are going from current flow.
 
-
+// To check whether that character is having Node or not.
     bool containsKey(char ch){
         return (links[ch-'a'] != nullptr);
     }
 
+// Adding the node and increase countPrefix by 1.
     void put(char ch, Node* node){
         links[ch - 'a'] = node;
         countPrefix++;
     }
 
+// Return the Node address.
     Node* get(char ch){
         return links[ch - 'a'];
     }
 
+// when word will end it will increase EndWith count.
     void setEnd(){
         endWith++;
     }
 
+// When containsKey return true during insert and same char not added that time it will increase countPrefix.
     void setPrefix(){
         countPrefix++;
     }
 
+// Reture how many words ending at that trie.
     int isEnd(){
         return endWith;
     }
 
+// At perticular trie it will tell No. of words going through.
     int getWordCount(){
         return countPrefix;
     }
 
+// When we erase word it will decrease countPrefix count.
     void erasePrefixCount(){
         countPrefix--;
     }
 
+// When entire word got deleted that time it will decrease endWith.
     void  eraseWordCount(){
         endWith--;
     }
@@ -53,6 +61,7 @@ class Trie{
         root = new Node();
     }
 
+// inserting word in Trie.
     void insert(string &word){
         Node* node = root;
         for(int i=0; i<word.size(); i++){
@@ -66,6 +75,7 @@ class Trie{
         node->setEnd();
     }
 
+// Counting entire word till end and return EndWith value.
     int countWordsEqualTo(string &word){
         Node* node = root;
         for(int i=0; i<word.size(); i++){
@@ -77,6 +87,7 @@ class Trie{
         return node->isEnd();
     }
 
+// Count word till given and return countPrefix value.
     int countWordsStartingWith(string &word){
         Node* node = root;
         for(int i=0; i<word.size(); i++){
@@ -88,6 +99,7 @@ class Trie{
         return node->getWordCount();
     }
 
+// Erase the word by dcrementing prefixCount in every trie node and at end decrease endWith value.
     void erase(string &word){
         Node* node = root;
         for(int i=0; i<word.size(); i++){
