98ac2441ba9acd81a2d74aff7731221a7109bf62
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-09-08 04:14:05 +0530

    Solved on CodingNinjas

---
+++ LeetCode/Floor from BST.cpp
@@ -0,0 +1,21 @@
+// Problem Link => https://www.codingninjas.com/studio/problems/floor-from-bst_625868?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+
+int Floor(BinaryTreeNode<int> *node, int input)
+{
+	int floor = -1;
+	while(node){
+		if(node->data == input){
+			floor = node->data;
+			return floor;
+		}
+		
+		if(input > node->data){
+			floor = node->data;
+			node = node->right;
+		}
+		else
+			node = node->left;
+	}
+	return floor;
+
+}
