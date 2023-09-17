a66775c4cdf6336a5437ff33e84b5459960fb12d
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-09-18 01:01:15 +0530

    practicing

--- LeetCode/temqp.cpp
+++ LeetCode/temqp.cpp
@@ -1,41 +1,8 @@
 #include<bits/stdc++.h>
-#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(){
-    int N,M;
-    cin>>N>>M;
-    int* arr = new int[M];
-    int fire[N];
-    for (int i = 0; i < N; i++)
-    {
-        fire[i] = -1;
-    }
-    for (int i = 0; i < M; i++)
-    {
-        cin>>arr[i];
-        fire[arr[i]-1] = 0;
-
-    }
-    int i=1,j=1;
-    while (i!=N)
-    {
-        if(fire[i-1] == -1){
-            fire[i-1] = arr[j-1] - i;
-            i++;
-        }
-        else{
-            i++;
-            j++;
-        }
-    }
-    for (int i = 0; i < N; i++)
-    {
-        cout<<fire[i]<<endl;
-    }
-    
-    
-
-
+    cout<<(1<<0);
     return 0;
 }
