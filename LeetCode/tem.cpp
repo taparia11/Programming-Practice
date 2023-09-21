8319afb15f23f909773a0badb186c5130c15c7e2
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-09-21 22:10:43 +0530

    Hackerrank Accenture contest srm Q3

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -1,43 +1,79 @@
-// C++ program to find the length of the longest substring
-// without repeating characters
+// A Naive C++ recursive implementation
+// of LIS problem
 #include <bits/stdc++.h>
 using namespace std;
 
-// This function returns true if all characters in str[i..j]
-// are distinct, otherwise returns false
-bool areDistinct(string str, int i, int j)
+// To make use of recursive calls, this
+// function must return two things:
+// 1) Length of LIS ending with element
+// arr[n-1].
+// We use max_ending_here for this purpose
+// 2) Overall maximum as the LIS may end
+// with an element before arr[n-1] max_ref
+// is used this purpose.
+// The value of LIS of full array of size
+// n is stored in *max_ref which is
+// our final result
+int _lis(int arr[], int n, int* max_ref)
 {
 
-    // Note : Default values in visited are false
-    vector<bool> visited(256);
+    // Base case
+    if (n == 1)
+        return 1;
 
-    for (int k = i; k <= j; k++) {
-        if (visited[str[k]] == true)
-            return false;
-        visited[str[k]] = true;
+    // 'max_ending_here' is length of
+    // LIS ending with arr[n-1]
+    int res, max_ending_here = 1;
+
+    // Recursively get all LIS ending with
+    // arr[0], arr[1] ... arr[n-2]. If
+    // arr[i-1] is smaller than arr[n-1],
+    // and max ending with arr[n-1] needs
+    // to be updated, then update it
+    for (int i = 1; i < n; i++) {
+        res = _lis(arr, i, max_ref);
+        if (arr[i - 1] < arr[n - 1]
+            && res + 1 > max_ending_here)
+            max_ending_here = res + 1;
     }
-    return true;
+
+    // Compare max_ending_here with the
+    // overall max. And update the
+    // overall max if needed
+    if (*max_ref < max_ending_here)
+        *max_ref = max_ending_here;
+
+    // Return length of LIS ending
+    // with arr[n-1]
+    return max_ending_here;
 }
 
-// Returns length of the longest substring
-// with all distinct characters.
-int longestUniqueSubsttr(string str)
+// The wrapper function for _lis()
+int lis(int arr[], int n)
 {
-    int n = str.size();
-    int res = 0; // result
-    for (int i = 0; i < n; i++)
-        for (int j = i; j < n; j++)
-            if (areDistinct(str, i, j))
-                res = max(res, j - i + 1);
-    return res;
+
+    // The max variable holds the result
+    int max = 1;
+
+    // The function _lis() stores its
+    // result in max
+    _lis(arr, n, &max);
+
+    // Returns max
+    return max;
 }
 
-// Driver code
+// Driver program to test above function
 int main()
 {
-    char str[100];
-    scanf("%s",str);
-    int len = longestUniqueSubsttr(str);
-    cout<<len;
+    int n;
+    cin>>n;
+    int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+    // int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Function call
+    cout << lis(arr, n);
     return 0;
 }
