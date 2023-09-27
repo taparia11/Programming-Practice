42722753ec71d45a5e9175fb8551e19d372eb44a
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-09-27 10:19:51 +0530

    Added comments

--- LeetCode/662. Maximum Width of Binary Tree.cpp
+++ LeetCode/662. Maximum Width of Binary Tree.cpp
@@ -17,13 +17,16 @@ public:
         unsigned int ans = 0;
         if(root==NULL)
             return ans;
+        // used pair to store node and index
         queue<pair<TreeNode*,unsigned int>> q;
         q.push({root,0});
         while(!q.empty()){
             unsigned int size = q.size();
+            // always give parent index = assign 1 as index number on every level
             unsigned int mmin = q.front().second;
             unsigned int first,last;
             for( int i=0;i<size;i++){
+                // through this curr we will assign index to chile nodes
                 unsigned int curr = q.front().second-mmin;
                 TreeNode* node = q.front().first;
                 q.pop();
@@ -33,6 +36,7 @@ public:
                 if(node->right!=NULL)    q.push({node->right,2*curr+2});
             
             }
+            // this will give us the all the nodes between first and last node using index number
             ans = max(ans,last-first+1);
         }
         return ans;
