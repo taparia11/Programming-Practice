c762bbafbcaacb8642b8af4ef328fed893301af0
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-09-29 21:35:08 +0530

    Solved for Amazon hackon contest

--- LeetCode/temqp.cpp
+++ LeetCode/temqp.cpp
@@ -1,28 +1,28 @@
 #include<bits/stdc++.h>
 #include<vector>
 using namespace std;
-
 int main(){
     int n;
     cin>>n;
-    int arr[n] = {0};
-    arr[0] = n-1;
-    for (int i = 0; i < n-1; i++)
-    {
-        int a;
-        cin>>a;
-        arr[a-1]++;
-        // cout<<a-1<<endl;
-
+    string s[n];
+    for (int i = 0; i < n; i++)
+        cin>>s[i];
+    vector<int> res;
+    for (int i = 0; i < n; i++)
+    {   int ans = 0,flag=0;
+        for (int j = 0; j < s[i].length(); j++)
+        {   if(s[i][j]>=48 && s[i][j]<=57){
+                ans  = ans*10 + (s[i][j] - 48);
+                flag =1;    
+            }
+        }
+        if(flag==0)
+            ans = s[i].length();
+        res.push_back(ans);
     }
-
-    cout<<n-1<<" ";
-
-    for (int i = 1; i < n; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    
-    
+    int ans = 1;
+    for(auto it:res)
+        ans = ans * it;
+    cout<<ans;
     return 0;
 }
