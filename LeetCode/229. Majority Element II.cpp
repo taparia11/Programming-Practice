72ccdf3f3fdf5a9cf77ce1d5ca9bc75144fc8fb1
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-02 02:26:01 +0530

    Solved on Leetcode and comment added

--- LeetCode/229. Majority Element II.cpp
+++ LeetCode/229. Majority Element II.cpp
@@ -3,12 +3,16 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+    // Creating unodered map to store key and value of numd ->  { value, Count}
         unordered_map<int, int> mapp;
+    //Applying the problem discription condition
         int size = nums.size()/3;
+    //inserting count of all elements in unordered map.
         for(int i=0;i<nums.size();i++)
             mapp[nums[i]]++;
-        
+    
         vector<int> ans;
+    // Iterating over every element of unordered_map to check the condition on count -> n/3 
         for(auto it = mapp.begin();it!=mapp.end();it++)
             if(it->second>size)
                 ans.push_back(it->first);
