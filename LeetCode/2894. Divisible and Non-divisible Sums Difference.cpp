60fc2037e442648f9203f5cee32a6e39256192ad
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-08 11:31:40 +0530

    Space Complexity optimixed

--- LeetCode/2894. Divisible and Non-divisible Sums Difference.cpp
+++ LeetCode/2894. Divisible and Non-divisible Sums Difference.cpp
@@ -3,20 +3,29 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-        vector<int> divisible;
-        vector<int> notDivisible;
-        for(int i=1; i<=n; i++){
-            if(i % m == 0)
-                divisible.push_back(i);
-            if(i % m != 0)
-                notDivisible.emplace_back(i);
+        // int num1 = 0, num2 = 0;
+        // vector<int> divisible;
+        // vector<int> notDivisible;
+        // for(int i=1; i<=n; i++){
+        //     if(i % m == 0)
+        //         divisible.push_back(i);
+        //     if(i % m != 0)
+        //         notDivisible.emplace_back(i);
         
-        }
-        for(auto it:divisible)
-            num2 += it;
-        for(auto it:notDivisible)
-            num1 += it;
-        return num1-num2;
+        // }
+        // for(auto it:divisible)
+        //     num2 += it;
+        // for(auto it:notDivisible)
+        //     num1 += it;
+        // return num1-num2;
+
+// Space Optimized Solution
+    int ans = 0;
+    for(int i=1; i<=n; i++)
+        if( i % m != 0)
+            ans += i;
+        else
+            ans -= i;
+    return ans;
     }
 };
---
+++ LeetCode/2895. Minimum Processing Time.cpp
@@ -0,0 +1,18 @@
+// Problem Link => https://leetcode.com/problems/minimum-processing-time/
+
+class Solution {
+public:
+    int minProcessingTime(vector<int>& processorTime, vector<int>& tasks) {
+        vector<int> cores;
+        for(auto it:processorTime)
+            for(int i=0; i<4; i++)
+                cores.push_back(it);
+        sort(cores.begin(), cores.end());
+        sort(tasks.begin(), tasks.end(), greater<int>());
+        int ans =0 ;
+        for(int i=0; i<tasks.size(); i++)
+            ans = max(ans, cores[i] + tasks[i]);
+        
+        return ans;
+    }
+};
