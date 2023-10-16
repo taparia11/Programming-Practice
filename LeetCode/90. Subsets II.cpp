29f30952abafe19710c981b9a809e308313fdb44
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-17 04:27:37 +0530

    commment added

--- LeetCode/90. Subsets II.cpp
+++ LeetCode/90. Subsets II.cpp
@@ -6,6 +6,7 @@ public:
     void subset(int idx, vector<int> &nums, vector<vector<int>> &ans, vector<int> &ds){
         ans.push_back(ds);
         for(int i=idx; i<nums.size(); i++){
+            // not to start again with same number
             if(i!=idx && nums[i] == nums[i-1])
                 continue;
             ds.push_back(nums[i]);
@@ -17,6 +18,7 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> ds;
+        // sorting so all element will get in increasing order that will make easy to not repeat same
         sort(nums.begin(),nums.end());
         subset(0, nums, ans, ds);
         return ans;
