337c7df98c4c4e46f02534755d1051595250679e
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-18 22:43:51 +0530

    Comment added

--- LeetCode/Subset Sum.cpp
+++ LeetCode/Subset Sum.cpp
@@ -1,18 +1,20 @@
 // Problem Link => https://www.codingninjas.com/studio/problems/subset-sum_3843086?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
 
 void subSequences( int idx, int n, vector<int> &values, vector<int> &res, int sum){
+  // If index == n then push sum into vector
   if(idx>=n){
     res.push_back(sum);
     return;
   }
+  //with adding Number
   sum += values[idx];
   subSequences(idx + 1, n, values, res, sum);
   sum -= values[idx];
+  //without adding Number
   subSequences(idx + 1, n, values, res, sum);
 }
 
 vector<int> subsetSum(vector<int> &num){
-	// Write your code here.
 	vector<int> ans;
 	subSequences(0, num.size(), num, ans, 0);
 	sort(ans.begin(), ans.end());
