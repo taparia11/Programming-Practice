44d09fba0de0f2f366c64835b694383e3a7300dc
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-19 12:20:40 +0530

    Solved on Leetcode optimized

--- LeetCode/46. Permutations.cpp
+++ LeetCode/46. Permutations.cpp
@@ -4,36 +4,55 @@ class Solution
 {
 public:
 
-    // With aditional storage of ds
-    void permutations(int n, vector<int> nums, vector<vector<int>> &ans, vector<int> &ds, int freq[])
-    {
-        if (ds.size() == n)
-        {
-            ans.push_back(ds);
+    // without additional storage
+    void permutations(int idx, vector<int> &nums, vector<vector<int>> &ans){
+        if(idx == nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for (int i = 0; i < n; i++)
-        {
-            if (!freq[i])
-            {
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                permutations(n, nums, ans, ds, freq);
-                freq[i] = 0;
-                ds.pop_back();
-            }
+        for(int i=idx; i<nums.size(); i++){
+            swap(nums[idx], nums[i]);
+            permutations(idx+1, nums, ans);
+            swap(nums[idx], nums[i]);
         }
     }
 
-    vector<vector<int>> permute(vector<int> &nums)
-    {
+    vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int n = nums.size();
-        int freq[n];
-        for (int i = 0; i < n; i++)
-            freq[i] = 0;
-        permutations(n, nums, ans, ds, freq);
+        permutations(0, nums, ans);
         return ans;
     }
+
+    // // With additional storage of ds
+    // void permutations(int n, vector<int> nums, vector<vector<int>> &ans, vector<int> &ds, int freq[])
+    // {
+    //     if (ds.size() == n)
+    //     {
+    //         ans.push_back(ds);
+    //         return;
+    //     }
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         if (!freq[i])
+    //         {
+    //             ds.push_back(nums[i]);
+    //             freq[i] = 1;
+    //             permutations(n, nums, ans, ds, freq);
+    //             freq[i] = 0;
+    //             ds.pop_back();
+    //         }
+    //     }
+    // }
+
+    // vector<vector<int>> permute(vector<int> &nums)
+    // {
+    //     vector<vector<int>> ans;
+    //     vector<int> ds;
+    //     int n = nums.size();
+    //     int freq[n];
+    //     for (int i = 0; i < n; i++)
+    //         freq[i] = 0;
+    //     permutations(n, nums, ans, ds, freq);
+    //     return ans;
+    // }
 };
