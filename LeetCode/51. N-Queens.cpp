189923f5d7ad3411bbba5ad00b6fc56d96a8fe9f
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-20 12:25:40 +0530

    Solved on Leetcode comment added

--- LeetCode/51. N-Queens.cpp
+++ LeetCode/51. N-Queens.cpp
@@ -6,13 +6,15 @@ public:
     bool isSafe(vector<string> &board, int col, int row, int n){
         int dupcol = col;
         int duprow = row;
-
+// checking Attack in three direction's 
+//  Right Side (Straight Right)
         while(col>=0){
             if(board[row][col]=='Q')
                 return false;
             col--;
         }
         col = dupcol;
+// Bottom-Right (Diagonal)
         while(col>=0 && row<n){
             if(board[row][col]=='Q')
                 return false;
@@ -21,23 +23,32 @@ public:
         }
         col = dupcol;
         row = duprow;
+// Top-Left (Diagonal)
         while(col>=0 && row>=0){
             if(board[row][col]=='Q')
                 return false;
             col--;
             row--;
         }
+        
         return true;
     }
 
     void solve(int col, vector<vector<string>> &ans, vector<string> &board, int n){
+        // if col reaches to n means a possible solution occured 
+        // if n = 4 then col will be [0, 1, 2, 3, 4] at col(4) == N(4) it will push that in to ans vector
         if(col == n){
             ans.push_back(board);
             return;
         }
 
+        //Iterating over the row's 
+        // Placing Queen on every row to check possible condition
+        // column will only increase only in new recurrision
         for(int row = 0; row < n; row++){
+            //Checking other queen's are attacking on this position or not 
             if(isSafe(board, col, row, n)){
+                //If no Queen attacking on this position then put Queen here
                 board[row][col] = 'Q';
                 solve(col+1, ans, board, n);
                 board[row][col] = '.'; 
@@ -48,10 +59,10 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
-        string s(n , '.');
+        string s(n , '.'); // Creating a string of length n  filling with '.'
         for(int i=0; i<n; i++)  
-            board[i] = s;
-        solve(0, ans, board, n);
+            board[i] = s; // marking each cell of board with '.'
+        solve(0, ans, board, n); // solve(Column_Number, Result, temprarory_board, Size_of_board)
         return ans;
     }
 };
