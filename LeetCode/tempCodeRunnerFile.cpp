e3b44bef967b5f084895c7e5ade28dbf394429fa
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-25 21:30:00 +0530

    Solved

--- LeetCode/tempCodeRunnerFile.cpp
+++ LeetCode/tempCodeRunnerFile.cpp
@@ -1,1 +1,22 @@
-ar+1
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int *ans = (int*)malloc(n*sizeof(int));
+    for(int i=0; i<n; i++){
+        cin>>ans[i];
+    }
+    int m = *max_element(ans, ans+n);
+    int sum=0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += ((ans[i]) * (i+1)) + m;
+    }
+
+    cout<<sum;
+    
+    return 0;
+}
