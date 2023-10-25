b6c1719acad0f6aaa43c842bbfc6d3a10392337e
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-25 21:29:30 +0530

    Solved for codechef contest

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -1,23 +1,32 @@
-#include<bits/stdc++.h>
-#include<vector>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    int *ans = (int*)malloc(n*sizeof(int));
-    for(int i=0; i<n; i++){
-        cin>>ans[i];
-    }
-    sort(ans, ans+n);
-    int m = *max_element(ans, ans+n);
-    int sum=0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += ((ans[i]) * (i+1)) + m;
-    }
-
-    cout<<sum;
-    
-    return 0;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+        int sum=0;
+        int *arr = (int*)malloc(n*sizeof(int));
+        for (int i = 0; i < n; i++)
+        {
+            cin>>arr[i];
+            sum += arr[i];
+        }
+         if(sum%2!=0){
+            cout<<"No"<<endl;
+            continue;
+         }
+         int temp = sum/n;
+         if(sum%n==0 && temp%2==0){
+            cout<<"Yes"<<endl;
+            continue;
+         }
+        
+	    cout<<"No"<<endl;
+	    
+	}
+	return 0;
 }
