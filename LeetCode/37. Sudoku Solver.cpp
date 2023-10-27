683a54dbbd5b74bbf9e809172f71b4e2931633bd
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-27 19:26:20 +0530

    Comment added

--- LeetCode/37. Sudoku Solver.cpp
+++ LeetCode/37. Sudoku Solver.cpp
@@ -3,13 +3,18 @@
 class Solution {
 public:
 
-    bool  solve(vector<vector<char>> &board){
+    bool solve(vector<vector<char>> &board){
+        //iterating over row
         for(int row = 0; row<board.size(); row++){
+        //iterating over col 
             for(int col=0; col<board[0].size(); col++){
+        //checking place is empty or not
                 if(board[row][col] == '.'){
                     for(char ch='1'; ch<='9'; ch++){
+        //checking that place can accept ch value or not
                         if(isValid(board , row, col, ch)){
                             board[row][col] = ch;
+        //after placing number checking for next place in recurrision
                             if(solve(board)==true)
                                 return true;
                             else 
@@ -25,10 +30,13 @@ public:
 
     bool isValid(vector<vector<char>> &board, int row, int col, char ch){
         for(int i=0; i<9; i++){
+        //ch not available in entire row
             if(board[row][i] == ch)
                 return false;
+        //ch not available in entire col
             if(board[i][col] == ch)
                 return false;
+        //ch not available in entire 3*3
             if(board[3 * (row/3) + i/3][3 * (col/3) + i%3] == ch)
                 return false;
         }
