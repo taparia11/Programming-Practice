09e6d2112cf0af6d024b60adbeadb2646c546fe8
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-28 18:44:15 +0530

    Solved for atcode contest Q2

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -1,23 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include<vector>
 using namespace std;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    sort(a.begin(), a.end());
-    int left = 0, right = 0, max_gifts = 0;
-    while (right < n) {
-        if (a[right] - a[left] < m) {
-            right++;
-        } else {
-            left++;
-        }
-        max_gifts = max(max_gifts, right - left);
-    }
-    cout << max_gifts << endl;
-    return 0;
+bool check(int n){
+	int one = n%10, prod = 1;
+	n /= 10;
+	while(n>0){
+		int rem = n%10;
+		prod *= rem;
+		n /= 10;
+	}
+	return one == prod;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	while(n){
+		if(check(n)){
+			cout<<n;
+			return 0;
+		}
+		if(n<919)
+			n++;
+	}
+	
+	return 0;
 }
