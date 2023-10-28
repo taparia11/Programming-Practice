63553f911946166fa7972e9f150f4f1af6ce1d82
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-28 18:43:41 +0530

    Solved for atcode contest Q3

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -1,21 +1,23 @@
-#include<bits/stdc++.h>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int x,y;
-	cin>>x>>y;
-	if(x>y){
-		if(x-y<=3)
-			cout<<"Yes";
-		else
-			cout<<"No";
-	}
-	else{
-		if(y-x<=2)
-			cout<<"Yes";
-		else
-			cout<<"No";
-	}
-	return 0;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int left = 0, right = 0, max_gifts = 0;
+    while (right < n) {
+        if (a[right] - a[left] < m) {
+            right++;
+        } else {
+            left++;
+        }
+        max_gifts = max(max_gifts, right - left);
+    }
+    cout << max_gifts << endl;
+    return 0;
 }
