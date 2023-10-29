a85a413e2b787dfe6881bede611ee49e1c04093a
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-10-29 17:30:52 +0530

    Leetcode problem comment added

--- LeetCode/60. Permutation Sequence.cpp
+++ LeetCode/60. Permutation Sequence.cpp
@@ -3,21 +3,32 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        //Storing numbers from 1 to n
         vector<int> number;
         int fact = 1;
+        //adding numbers into vector and calculating factorial till n-1
         for(int i=1; i<n; i++){
             fact = fact * i;
             number.push_back(i);
         }
         number.push_back(n);
         string ans = "";
+        // k = k-1 because 0-base Indexing
         k = k-1;
         while(true){
+                
+            // position of element  =  k / fact
             ans = ans + to_string(number[k/fact]);
             number.erase(number.begin() + k / fact);
             if(number.size()==0)
                 break;
+            // new k = k % fact
             k = k%fact;
+            // updating fact to new number size in vector (here size is decreasing by 1 every time in vector
+            //  so we dividing it with number size)
+            //  n = 4 fact = 6
+            //  n = 3 fact = 6/3 = 2
+            //  n = 2 fact = 2/2 = 1
             fact = fact / number.size();
         }
         return ans;
