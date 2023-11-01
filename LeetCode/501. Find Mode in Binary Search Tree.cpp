cf8b283f8d87cf5d58d1072da0af9d7ef68a297a
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-11-01 17:38:53 +0530

    Comment added

--- LeetCode/501. Find Mode in Binary Search Tree.cpp
+++ LeetCode/501. Find Mode in Binary Search Tree.cpp
@@ -11,29 +11,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 private:
+    //Storing the value's
     vector<int> mode;
-    int currCount = 0;
-    int currVal = 0;
-    int maxCount = 0;
+    int currCount = 0; // how many times same number repeated
+    int currVal = 0;  // current value
+    int maxCount = 0;   // max time a number occured
 
-    void inOrder(TreeNode* root){
-        if(!root)
+    void inOrder(TreeNode *root)
+    {
+        if (!root)
             return;
         inOrder(root->left);
-        currCount = (root->val == currVal? currCount+1:1);
-        if(currCount == maxCount)
-            mode.emplace_back(root->val);
-        else if(currCount > maxCount){
+        // if Node value is rqual to current value then increase current value count with 1 else set 1
+        currCount = (root->val == currVal ? currCount + 1 : 1);
+        // if current value count equal to max count means this number also occured same number of  time as max
+        if (currCount == maxCount)
+            mode.emplace_back(root->val); 
+        // current value count > max count
+        else if (currCount > maxCount)
+        {
+            // set max count = current count
             maxCount = currCount;
+            // set vector to node value
             mode = {root->val};
         }
+        // set current value equal to node value
         currVal = root->val;
         inOrder(root->right);
     }
+
 public:
-    vector<int> findMode(TreeNode* root) {
+    vector<int> findMode(TreeNode *root)
+    {
         inOrder(root);
         return mode;
     }
