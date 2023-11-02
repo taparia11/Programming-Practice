0bc99d107c70dc0432efd98981754f0f632cc65b
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-11-02 22:28:33 +0530

    Solved

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -2,11 +2,58 @@
 #include<vector>
 using namespace std;
 
+vector<int> prim;
+void primeFactors(int n)  
+{  
+    for (int i = 3; i <= n; i = i + 2)  
+    { 
+        while (n % i == 0)  
+        {  
+            n = n/i;  
+
+            prim.push_back(i);  
+        }  
+    }  
+    if (n > 2)  
+            prim.push_back(n);  
+}  
+  
+
 int main(){
-	vector<int> ans;
-	ans = {5};
-	ans = {7};
-	for(auto it:ans)
-		cout<<it<<endl;	
+	int t;
+	cin>>t;
+	while (t--)
+	{
+		int n;
+		cin>>n;
+		primeFactors(n);
+		int alice=0, bob=0;
+		for(auto it=prim.rbegin();it!=prim.rend();){
+			if(alice<=bob){
+				if((n- (*it))<0)
+					break;
+				n = n- (*it);
+				alice++;
+			}
+			else{
+				if((n- (*it))<0)
+					break;
+				n = n- (*it);
+				bob++;
+			}
+			if(n==0)
+				break;
+			if((*it)>n)
+				it++;
+		}
+
+		prim.clear();
+		if(alice==bob)
+			cout<<"Bob"<<endl;
+		else
+			cout<<"Alice"<<endl;
+
+	}
+	
 	return 0;
 }
