3589184089cc7b7527c96a109c3219ff5dd13551
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-11-08 23:37:10 +0530

    Practice contest

--- LeetCode/tem.cpp
+++ LeetCode/tem.cpp
@@ -1,22 +1,45 @@
-#include<bits/stdc++.h>
-#include<vector>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-long getScoreDifference(vector<int> points){
-	priority_queue<int> p;
-	for(int i=0; i<points.size(); i++){
-		p.push(points[i]);
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+		string chefp, chefinap;
+		cin>>chefp;
+		cin>>chefinap;
+		int chef = 0, chefina = 0, draw=0;
+		for(int i=0; i<n; i++){
+			// S s r p
+			// P s r p
+			// R s r p
+			if(chefp[i] == 'S' && chefinap[i] == 'P')
+				chef++;
+			else if(chefp[i] == 'P' && chefinap[i] == 'R')
+				chef++;
+			else if(chefp[i] == 'R' && chefinap[i] == 'S')
+				chef++;
+			else if(chefp[i] == chefinap[i])
+				draw++;
+		}
+		chefina = n - draw - chef;
+		if(chef>chefina)
+	    	cout<<0<<endl;
+		else if(chef==chefina)
+	    	cout<<1<<endl;
+		else{
+			draw = 0;
+			while(chef <= chefina){
+				draw++;
+				chef++;
+				chefina--;
+			}
+			cout<<draw<<endl;
+		}
+	    
 	}
-	return p.top();
-}
-
-int main(){
-	int n;
-	cin>>n;
-	vector<int> points(n);
-	for(int i=0; i<n; i++){
-		cin>>points[i];
-	}
-	cout<<getScoreDifference(points);
 	return 0;
 }
