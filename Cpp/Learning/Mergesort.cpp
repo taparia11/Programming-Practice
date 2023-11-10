6a7be519b921877c688407040d2a67ac3a03fdf8
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-11-10 23:50:15 +0530

    merge sort comment added

--- Cpp/Learning/Mergesort.cpp
+++ Cpp/Learning/Mergesort.cpp
@@ -3,26 +3,31 @@
 using namespace std;
 
 void merge(int arr[], int l, int mid, int r){
+    // size of left side
     int size1 = mid - l + 1;
+    // size of right side
     int size2 = r - mid;
-    
+    // left array
     int a[size1];
+    // right array
     int b[size2];
-
+    // storing left array from main array
     for (int i = 0; i < size1; i++)
     {
         a[i] = arr[l+i];
     }
     
+    // storing right array from main array
     for (int i = 0; i < size2; i++)
     {
         b[i] = arr[mid + 1 + i];
     }
 
     int i=0, j=0, k=l;
-
+    
     while (i<size1 && j<size2)
     {
+        //rearranging in small to large
         if(a[i] < b[j]){
             arr[k] = a[i];
             k++;
@@ -34,7 +39,7 @@ void merge(int arr[], int l, int mid, int r){
             j++;
         }
     }
-
+    // remainning elements in left array
     while (i<size1)
     {
             arr[k] = a[i];
@@ -42,6 +47,7 @@ void merge(int arr[], int l, int mid, int r){
             i++; 
     }
     
+    // remainning elements in right array
     while (j<size2)
     {
             arr[k] = b[j];
@@ -52,10 +58,13 @@ void merge(int arr[], int l, int mid, int r){
 
 void mergeSort(int arr[], int l, int r){
     if(l<r){
+        // taking half of array
         int mid = (l+r)/2;
+        // from left to mid
         mergeSort(arr, l, mid);
+        // from mid + 1 to right
         mergeSort(arr, mid+1, r);
-
+        // merging array
         merge(arr, l, mid, r);
 
     }
